99ee6547f2aa73da984d9081e83ba79f60d8e3a3
Colin Wilson <furious.gecko@gmail.com>
2026-04-02 16:33:40 +0100

    added a script file which does everything

---
+++ arch_post_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# install Yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf yay-bin
+
+# install assorted software
+yay -S --noconfirm waterfox-bin pacseek-bin dropbox nautilus-dropbox spotify papirus-folders
+sudo pacman -S --noconfirm adw-gtk-theme baobab bash-completion btop btrfs-assistant cava decibels dosfstools fd ffmpegthumbnailer file-roller fzf gcc github-cli gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gstreamer gst-plugin-pipewire gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gvfs-smb kimageformats libappindicator libdvdcss libgsf libopenraw libreoffice-fresh-en-gb loupe man-db neovim ntfs-3g obsidian papers papirus-icon-theme plocate poppler-glib vlc-plugins-all power-profiles-daemon qt5ct qt6ct ripgrep showtime snapshot starship sushi tree-sitter-cli wl-clipboard xdg-user-dirs xdg-utils
+#sudo pacman -S --noconfirm steam
+
+# install fonts
+sudo pacman -S --noconfirm noto-fonts-emoji cantarell-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans ttf-meslo-nerd noto-fonts-cjk ttf-firacode-nerd
+yay -S --noconfirm ttf-ms-fonts
+
+# remove redundant programs
+sudo pacman -R --noconfirm nano vim network-manager-applet waybar gpsd ly htop
+
+# install DankMaterialShell for Niri:
+curl -fsSL https://install.danklinux.com | sh
+
+# edit shell config
+echo '' >>~/.bashrc
+echo 'export EDITOR=nvim' >>~/.bashrc
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+
+# enable gnome-keyring unlocking on log-in:
+echo '#%PAM-1.0' | sudo tee /etc/pam.d/greetd >/dev/null
+echo 'auth required pam_securetty.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'auth requisite pam_nologin.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'auth include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'auth optional pam_gnome_keyring.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'account include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'session include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
+echo 'session optional pam_gnome_keyring.so auto_start' | sudo tee -a /etc/pam.d/greetd >/dev/null
