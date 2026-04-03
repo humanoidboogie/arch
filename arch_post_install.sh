339eadae664ebd2226dbe426ddb69a671f2b9b7b
Colin Wilson <furious.gecko@gmail.com>
2026-04-03 17:37:13 +0100

    simplified codecs

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -12,8 +12,8 @@ rm -rf yay-bin
 
 # install assorted software
 yay -S --noconfirm waterfox-bin pacseek-bin dropbox nautilus-dropbox spotify papirus-folders vscodium-bin
-sudo pacman -S --noconfirm adw-gtk-theme baobab bash-completion btop btrfs-assistant cava decibels dosfstools fd ffmpegthumbnailer file-roller fzf gcc github-cli gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gstreamer gst-plugin-pipewire gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gvfs-smb kimageformats libappindicator libdvdcss libgsf libopenraw libreoffice-fresh-en-gb loupe man-db neovim ntfs-3g obsidian papers papirus-icon-theme poppler-glib vlc-plugins-all power-profiles-daemon qt5ct qt6ct ripgrep showtime snapshot starship sushi tree-sitter-cli wl-clipboard xdg-user-dirs xdg-utils
-#sudo pacman -S --noconfirm steam
+sudo pacman -S --noconfirm adw-gtk-theme baobab bash-completion btop btrfs-assistant cava decibels dosfstools fd ffmpeg file-roller fzf gcc github-cli gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-smb kimageformats libappindicator libgsf libreoffice-fresh-en-gb loupe man-db neovim ntfs-3g obsidian papers papirus-icon-theme power-profiles-daemon qt5ct qt6ct ripgrep showtime snapshot starship sushi tree-sitter-cli wl-clipboard xdg-user-dirs xdg-utils
+sudo pacman -S --noconfirm steam
 
 # install fonts
 sudo pacman -S --noconfirm noto-fonts-emoji cantarell-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans ttf-meslo-nerd noto-fonts-cjk ttf-firacode-nerd
@@ -44,9 +44,9 @@ echo 'session include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/
 echo 'session optional pam_gnome_keyring.so auto_start' | sudo tee -a /etc/pam.d/greetd >/dev/null
 
 # setup limine sync for snapper
-sudo rm /boot/limine/limine.conf
-yay -S --noconfirm limine-snapper-sync limine-mkinitcpio-hook
-sudo pacman -S --noconfirm snap-pac
+#sudo rm /boot/limine/limine.conf
+#yay -S --noconfirm limine-snapper-sync limine-mkinitcpio-hook
+#sudo pacman -S --noconfirm snap-pac
 
 # workaround for vscodium changing the default directory handler
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
