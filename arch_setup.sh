e70a938818fe2a9b9e62c4e8cf20e4feed9a75f7
Colin Wilson <furious.gecko@gmail.com>
2026-04-15 18:11:32 +0100

    removed sushi and renamed script file

--- arch_setup.sh
+++ arch_setup.sh
@@ -7,10 +7,10 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 
-# bring in gnome base
-yay -Sy --noconfirm adw-gtk-theme baobab bash-completion btrfs-assistant cava decibels dosfstools dropbox dsearch  fd ffmpeg4.4 file-roller freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacseek-bin papers papirus-folders power-profiles-daemon qt5ct qt6ct qt6-multimedia ripgrep showtime snap-pac spotify starship steam sushi tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
+# install software
+yay -Sy --noconfirm adw-gtk-theme baobab bash-completion btrfs-assistant cava decibels dosfstools dropbox dsearch fd ffmpeg4.4 file-roller freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacseek-bin papers papirus-folders power-profiles-daemon qt5ct qt6ct qt6-multimedia ripgrep showtime snap-pac spotify starship steam tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
 
-# install DankMaterialShell for Niri:
+# install DankMaterialShell:
 curl -fsSL https://install.danklinux.com | sh
 
 # edit shell config
