8d1f60e777f65e2f6b51c5af81457bd491812520
Colin Wilson <furious.gecko@gmail.com>
2026-04-15 10:09:26 +0100

    simplified install script

--- arch_post_install_headless.sh
+++ arch_post_install_headless.sh
@@ -8,25 +8,7 @@ cd ..
 rm -rf yay-bin
 
 # bring in gnome base
-sudo pacman -S --noconfirm xdg-desktop-portal-gnome
-
-# install niri
-sudo pacman -S --noconfirm mako niri swaybg swayidle swaylock xorg-xwayland
-
-# bring in quickshell and build
-yay -S --noconfirm quickshell-git
-
-# install assorted software
-yay -S --noconfirm waterfox-bin pacseek-bin dropbox nautilus-dropbox spotify papirus-folders vscodium-bin
-sudo pacman -S --noconfirm adw-gtk-theme baobab bash-completion btop btrfs-assistant cava decibels dosfstools fd ffmpeg4.4 file-roller fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db neovim ntfs-3g obsidian papers power-profiles-daemon qt5ct qt6ct ripgrep showtime snapshot starship sushi tree-sitter-cli wl-clipboard xdg-utils
-sudo pacman -S --noconfirm steam
-
-# install fonts
-sudo pacman -S --noconfirm noto-fonts-emoji noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans noto-fonts-cjk ttf-firacode-nerd ttf-roboto
-yay -S --noconfirm ttf-ms-fonts
-
-# remove extra fonts we don't need
-sudo pacman -Rsn --noconfirm gnu-free-fonts
+yay -Sy --noconfirm adw-gtk-theme baobab bash-completion btrfs-assistant cava decibels dosfstools dropbox dsearch  fd ffmpeg4.4 file-roller freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacseek-bin papers papirus-folders power-profiles-daemon qt5ct qt6ct qt6-multimedia ripgrep showtime snap-pac spotify starship steam sushi tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
 
 # install DankMaterialShell for Niri:
 curl -fsSL https://install.danklinux.com | sh
@@ -51,8 +33,5 @@ xdg-user-dirs-update
 
 # workaround for vscodium changing the default directory handler
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
-
-# install snap-pac to trigger snapshots on package installs
-sudo pacman -S --noconfirm snap-pac
 
 echo "Installation done. Remember to install nvidia drivers if not done already."
