6fc876604993e30da07fa0ad60bdd9d5e8adaf50
Colin Wilson <furious.gecko@gmail.com>
2026-04-20 23:51:51 +0100

    removed limine packages

--- arch_setup.sh
+++ arch_setup.sh
@@ -7,14 +7,8 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 
-# remove initial limine config file
-sudo rm /boot/limine/limine.conf
-
 # install software
-yay -Sy --noconfirm adw-gtk-theme baobab bash-completion btrfs-assistant cava cliphist decibels dosfstools dropbox dsearch fd ffmpeg4.4 file-roller firefox freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb limine-mkinitcpio-hook limine-snapper-sync loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin papers papirus-folders power-profiles-daemon qt6ct qt6-multimedia ripgrep showtime snap-pac spotify starship steam tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
-
-# remove jdk stuff after building limine-snapper-sync and limine-mkinitcpio-hook
-sudo pacman -Rsn gradle
+yay -Sy --noconfirm adw-gtk-theme baobab bash-completion btrfs-assistant cava cliphist decibels dosfstools dropbox dsearch fd ffmpeg4.4 file-roller firefox freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin papers papirus-folders power-profiles-daemon qt6ct qt6-multimedia ripgrep showtime snap-pac spotify starship tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
 
 # install DankMaterialShell:
 curl -fsSL https://install.danklinux.com | sh
