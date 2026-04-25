229bb6e86fd114b0a5908a7d4fb61a1913222b45
Colin Wilson <furious.gecko@gmail.com>
2026-04-25 09:44:41 +0100

    removed bashrc create. copy greetd file instead of creating it.

--- arch_setup.sh
+++ arch_setup.sh
@@ -8,33 +8,21 @@ cd ..
 rm -rf yay-bin
 
 # install software
-yay -Sy --noconfirm --needed adw-gtk-theme baobab bash-completion cava cliphist decibels dosfstools dropbox dsearch fd ffmpeg4.4 file-roller freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin papers papirus-folders power-profiles-daemon qt6ct qt6-multimedia ripgrep showtime spotify starship tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin waterfox-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
+yay -Sy --noconfirm --needed adw-gtk-theme baobab bash-completion cava cliphist decibels dosfstools dropbox dsearch fd ffmpeg4.4 file-roller freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nautilus-dropbox noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin papers papirus-folders power-profiles-daemon qt6ct qt6-multimedia ripgrep showtime snapshot spotify starship tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin waterfox-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
 
 # install DankMaterialShell:
 curl -fsSL https://install.danklinux.com | sh
 
-# edit shell config
-echo '' >>~/.bashrc
-echo 'export EDITOR=nvim' >>~/.bashrc
-echo 'eval "$(starship init bash)"' >>~/.bashrc
-
 # enable gnome-keyring unlocking on log-in:
-echo '#%PAM-1.0' | sudo tee /etc/pam.d/greetd >/dev/null
-echo 'auth required pam_securetty.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'auth requisite pam_nologin.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'auth include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'auth optional pam_gnome_keyring.so' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'account include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'session include system-local-login' | sudo tee -a /etc/pam.d/greetd >/dev/null
-echo 'session optional pam_gnome_keyring.so auto_start' | sudo tee -a /etc/pam.d/greetd >/dev/null
+sudo cp greetd /etc/pam.d/
 
 # setup home folders
 xdg-user-dirs-update
 
-# workaround for vscodium changing the default directory handler
+# the vscodium install changes the default directory handler. change it back!
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 
-# enable paccache timer
+# enable paccache timer for auto cleanup
 sudo systemctl enable --now paccache.timer
 
-echo "Installation done. Remember to install nvidia drivers if not done already."
+echo "Installation done. Remember to install graphics drivers if not done already."
