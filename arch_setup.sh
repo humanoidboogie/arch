b3670b342a5e68a3cd3c12a4414c290d0853f064
Colin Wilson <furious.gecko@gmail.com>
2026-05-25 21:03:30 +0100

    latest install script changes

--- arch_setup.sh
+++ arch_setup.sh
@@ -3,12 +3,15 @@
 # install Yay
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si --noconfirm
+makepkg -si
 cd ..
 rm -rf yay-bin
 
+# remove default limine config
+sudo rm /boot/limine/limine.conf
+
 # install software
-yay -Sy --noconfirm --needed adw-gtk-theme baobab bash-completion btrfs-assistant cava cliphist decibels dosfstools dropbox dsearch fastfetch fd ffmpeg4.4 file-roller fish freetype2 fzf gnome-calculator gnome-tweaks gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb limine-snapper-sync limine-mkinitcpio-hook loupe man-db nautilus-dropbox neovim noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin pandoc papers papirus-folders plymouth plymouth-theme-archlinux pomo-bin power-profiles-daemon qt6ct-kde qt6-multimedia reflector-simple ripgrep showtime snapshot spotify starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin waterfox-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
+yay -Sy --needed adw-gtk-theme baobab bash-completion cava cliphist decibels dosfstools dropbox dsearch fastfetch fd ffmpeg4.4 file-roller firefox fish freetype2 fzf gnome-calculator gnome-disk-utility gnome-keyring gst-libav gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb limine-mkinitcpio-hook loupe man-db nautilus-dropbox neovim noto-fonts noto-fonts-emoji noto-fonts-cjk ntfs-3g obsidian pacman-contrib pacseek-bin pandoc papers papirus-folders plymouth plymouth-theme-archlinux power-profiles-daemon qt6ct-kde qt6-multimedia reflector-simple ripgrep showtime snapshot spotify starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-ms-fonts ttf-opensans ttf-roboto xdg-utils vscodium-bin wl-clipboard xdg-desktop-portal-gnome xwayland-satellite
 
 # install DankMaterialShell:
 curl -fsSL https://install.danklinux.com | sh
@@ -24,8 +27,5 @@ xdg-mime default org.gnome.Nautilus.desktop inode/directory
 
 # enable paccache timer for auto cleanup
 sudo systemctl enable --now paccache.timer
-
-# install snap-pac to make snapshots after package (un)installs
-sudo pacman -Sy --noconfirm --needed snap-pac
 
 echo "Installation done. Remember to install graphics drivers if not done already."
