ce14853b3609a587cedefcb84746e00e5402e195
Colin Wilson <furious.gecko@gmail.com>
2026-06-17 15:40:25 +0100

    swapped out some packages

--- arch_setup.sh
+++ arch_setup.sh
@@ -11,10 +11,7 @@ cd ..
 rm -rf yay-bin
 
 # install software
-sudo pacman -Sy --needed 7zip avahi adw-gtk-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools fastfetch fd ffmpeg4.4 file-roller fish flatpak freetype2 fzf github-cli gnome-calculator gnome-disk-utility gnome-keyring gst-libav gvfs-dnssd gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db neovim noto-fonts noto-fonts-emoji noto-fonts-cjk nss-mdns ntfs-3g obsidian pacman-contrib pandoc papers plymouth power-profiles-daemon qt6-multimedia qt6ct ripgrep showtime snapshot starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-opensans ttf-roboto wl-clipboard xdg-desktop-portal-gnome xdg-utils xwayland-satellite
-
-# install AUR packages
-yay -Sy --needed nautilus-dropbox visual-studio-code-bin
+sudo pacman -Sy --needed 7zip avahi adw-gtk-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools fastfetch fd ffmpeg4.4 file-roller fish flatpak freetype2 fzf github-cli gnome-calculator gnome-disk-utility gnome-keyring gst-libav gvfs-dnssd gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en kimageformats libappindicator libreoffice-fresh-en-gb linux-headers loupe man-db neovim noto-fonts noto-fonts-emoji noto-fonts-cjk nss-mdns ntfs-3g obsidian pacman-contrib pandoc papers power-profiles-daemon qt6-multimedia qt6ct ripgrep showtime snapshot starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-jetbrains-mono-nerd ttf-liberation ttf-opensans ttf-roboto wl-clipboard xdg-desktop-portal-gnome xdg-utils xwayland-satellite
 
 # install DankMaterialShell:
 curl -fsSL https://install.danklinux.com | sh
@@ -34,4 +31,4 @@ sudo cp nsswitch.conf /etc/nsswitch.conf
 
 echo "Installation done. Remember to install graphics drivers if not done already."
 echo "nVidia: libva-nvidia-driver nvidia-open-dkms"
-echo "Intel: intel-media-driver libva-intel-driver vulkan-intel"
+echo "Intel: intel-media-driver libva-intel-driver vulkan-intel thermald"
