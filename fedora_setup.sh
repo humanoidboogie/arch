516143b687aa680e2c1c913a7a0797af0e1f5233
Colin Wilson <furious.gecko@gmail.com>
2026-06-20 19:31:09 +0100

    changes to fedora script

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -5,7 +5,7 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
 # install software
-sudo dnf install 7zip @multimedia avahi adw-gtk3-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools fastfetch fd file-roller firefox fish flatpak fzf gh gnome-calculator gnome-disk-utility gnome-keyring gnome-keyring-pam google-noto-emoji-fonts google-noto-sans-cjk-fonts google-noto-serif-cjk-fonts gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb hunspell-en-GB hyphen-en kf6-kimageformats libappindicator loupe man-db nautilus neovim nss-mdns ntfs-3g pandoc papers papirus-icon-theme plymouth plymouth-theme-spinner power-profiles-daemon qt6-qtmultimedia qt6ct ripgrep showtime snapshot stow tree-sitter-cli bitstream-vera-fonts-all dejavu-fonts-all wl-clipboard xdg-desktop-portal-gnome xdg-utils xdg-user-dirs xwayland-satellite
+sudo dnf install 7zip @multimedia avahi adw-gtk3-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools fastfetch fd file-roller fish flatpak fzf gh gnome-calculator gnome-disk-utility gnome-keyring gnome-keyring-pam google-noto-emoji-fonts google-noto-sans-cjk-fonts google-noto-serif-cjk-fonts gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb hunspell-en-GB hyphen-en jetbrains-mono-fonts kf6-kimageformats libappindicator loupe man-db nautilus neovim nss-mdns ntfs-3g pandoc papers papirus-icon-theme plymouth plymouth-theme-spinner power-profiles-daemon qt6-qtmultimedia qt6ct ripgrep showtime snapshot stow tree-sitter-cli bitstream-vera-fonts-all dejavu-fonts-all wl-clipboard xdg-desktop-portal-gnome xdg-utils xdg-user-dirs xwayland-satellite
 
 # codecs
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
@@ -23,6 +23,9 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # setup home folders
 xdg-user-dirs-update
 
-echo "Installation done. Remember to install graphics drivers if not done already."
+# make sure everything is up to date
+sudo dnf up
+
+echo "Installation done. Remember to install drivers if not done already."
 echo "Desktop: akmod-nvidia mt7xxx-firmware"
 echo "Laptop: iwlwifi-mvm-firmware intel-media-driver libva-intel-driver"
