b067c13f4858d10c10a78b498e171b8cfeeb0499
Colin Wilson <furious.gecko@gmail.com>
2026-08-13 16:31:15 +0100

    removed dms

--- arch_setup.sh
+++ arch_setup.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# install packages for building AUR
-sudo pacman -Sy --needed git base-devel
-
-# install Yay
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd ..
-rm -rf yay-bin
-
 # install software
-sudo pacman -Sy --needed 7zip avahi adw-gtk-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools faac fastfetch fd ffmpeg4.4 file-roller fish flatpak freetype2 fzf github-cli gnome-calculator gnome-disk-utility gnome-keyring gst-libav gvfs-dnssd gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en inter-font kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db neovim nss-mdns ntfs-3g obsidian pacman-contrib papers papirus-icon-theme power-profiles-daemon qt6-multimedia qt6ct ripgrep showtime snapshot starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-fira-code ttf-jetbrains-mono-nerd ttf-liberation ttf-opensans ttf-roboto wget wl-clipboard xdg-desktop-portal-gnome xdg-utils xwayland-satellite
-
-# install DankMaterialShell:
-curl -fsSL https://install.danklinux.com | sh
-
-# enable gnome-keyring unlocking on log-in:
-sudo cp greetd /etc/pam.d/
+sudo pacman -Sy --needed 7zip avahi adw-gtk-theme baobab bash-completion cava cliphist dconf-editor decibels dosfstools faac fastfetch fd ffmpeg4.4 file-roller fish flatpak freetype2 fzf github-cli gnome-calculator gnome-disk-utility gnome-keyring gst-libav gvfs-dnssd gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-wsdd hunspell-en_gb hyphen-en inter-font kimageformats libappindicator libreoffice-fresh-en-gb loupe man-db nss-mdns ntfs-3g obsidian pacman-contrib papers papirus-icon-theme power-profiles-daemon qt6-multimedia qt6ct ripgrep showtime snapshot starship stow tree-sitter-cli ttf-bitstream-vera ttf-dejavu ttf-fira-code ttf-jetbrains-mono-nerd ttf-liberation ttf-opensans ttf-roboto wget wl-clipboard xdg-desktop-portal-gnome xdg-utils xwayland-satellite
 
 # enable paccache timer for auto cleanup
 sudo systemctl enable --now paccache.timer
